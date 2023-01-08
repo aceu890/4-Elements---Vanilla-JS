19c6b5ac9d467e18a38dfc1d12403b731813353c
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2023-01-08 01:16:43 -0300

    refactor js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,34 @@
+window.addEventListener('load', () => {
+    const idImg_fuego = document.getElementById('id-img-fuego'),
+        idImg_agua = document.getElementById('id-img-agua'),
+        idImg_tierra = document.getElementById('id-img-tierra'),
+        idImg_aire = document.getElementById('id-img-aire'),
+        idFuego = document.getElementById('btn_id-fuego'),
+        idAgua = document.getElementById('btn_id-agua'),
+        idTierra = document.getElementById('btn_id-tierra'),
+        idAire = document.getElementById('btn_id-aire');
 
-// HTML References
+        const refactorFuncion = (aire,tierra,agua,fuego ) => { 
+            idImg_aire.style.display = aire;
+            idImg_tierra.style.display = tierra;
+            idImg_agua.style.display = agua;
+            idImg_fuego.style.display = fuego;
+        };
 
-// ID_IMAGES
-const idImg_fuego = document.getElementById('id-img-fuego');
-const idImg_agua = document.getElementById('id-img-agua');
-const idImg_tierra = document.getElementById('id-img-tierra');
-const idImg_aire = document.getElementById('id-img-aire');
+    idFuego.addEventListener('click', () => {
+        refactorFuncion('none','none','none','block');
+    });
 
+    idAgua.addEventListener('click', () => {
+        refactorFuncion('none','none','block','none');
+    });
+    idTierra.addEventListener('click', () => {
+        refactorFuncion('none','block','none','none');
+    });
 
-// ID-BTN
-const idFuego = document.getElementById('btn_id-fuego');
-const idAgua = document.getElementById('btn_id-agua');
-const idTierra = document.getElementById('btn_id-tierra');
-const idAire = document.getElementById('btn_id-aire');
-
-
-idFuego.addEventListener('click', () => {
-    idImg_aire.style.display = 'none';
-    idImg_tierra.style.display = 'none';
-    idImg_agua.style.display = 'none';
-
-    idImg_fuego.style.display = 'block';
-
-});
-
-idAgua.addEventListener('click', () => {
-    idImg_aire.style.display = 'none';
-    idImg_tierra.style.display = 'none';
-    idImg_fuego.style.display = 'none';
-
-    idImg_agua.style.display = 'block';
-});
-
-idTierra.addEventListener('click', () => {
-    idImg_aire.style.display = 'none';
-    idImg_fuego.style.display = 'none';
-    idImg_agua.style.display = 'none';
-    
-    idImg_tierra.style.display = 'block';
-});
-
-idAire.addEventListener('click', () => {
-    idImg_fuego.style.display = 'none';
-    idImg_tierra.style.display = 'none';
-    idImg_agua.style.display = 'none';
-
-    idImg_aire.style.display = 'block';
+    idAire.addEventListener('click', () => {
+        refactorFuncion('block','none','none','none');
+    });
 });
 
 
